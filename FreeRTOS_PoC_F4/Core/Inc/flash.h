be33a2b9c6d7d1ef7bda95f693b1aab890cdb190
Alexander <aperkals@gmail.com>
2026-06-23 13:20:46 +0300

    Added skeleton Flash driver code - need to test it

---
+++ FreeRTOS_PoC_F4/Core/Inc/flash.h
@@ -0,0 +1,59 @@
+/*
+ * flash.h
+ *
+ * Flash driver for W25Q16 serial Flash
+ *
+ */
+
+#ifndef _FLASH_H_
+#define _FLASH_H_
+
+void FlashReset(void);
+uint32_t W25Q_ReadID(void);
+
+// Flash commands
+#define FLASH_CMD_WRITE_ENABLE				0x06
+#define FLASH_CMD_VOL_SR_WRITE_ENABLE	0x50
+#define FLASH_CMD_WRITE_DISABLE				0x04
+
+#define FLASH_CMD_RELEASE_PWR_DOWN		0xAB
+#define FLASH_CMD_MANUF_ID						0x90
+#define FLASH_CMD_READ_JEDEC_ID				0x9F
+#define FLASH_CMD_READ_UNIQUE_ID			0x4B
+
+#define FLASH_CMD_READ_DATA						0x03
+#define FLASH_CMD_FAST_READ						0x03
+
+#define FLASH_CMD_PAGE_PROGRAM				0x02
+
+#define FLASH_CMD_BLK_ERASE_64K				0xD8
+#define FLASH_CMD_BLK_ERASE_32K				0x52
+#define FLASH_CMD_SECT_ERASE_4K				0x20
+#define FLASH_CMD_CHIP_ERASE					0xC7
+
+#define FLASH_CMD_READ_STAT_REG_1			0x05
+#define FLASH_CMD_WRITE_STAT_REG_1		0x05
+#define FLASH_CMD_READ_STAT_REG_2			0x35
+#define FLASH_CMD_WRITE_STAT_REG_2		0x31
+#define FLASH_CMD_READ_STAT_REG_3			0x15
+#define FLASH_CMD_WRITE_STAT_REG_3		0x11
+
+#define FLASH_CMD_READ_SFDP_REG				0x5A
+#define FLASH_CMD_ERASE_SEC_REG				0x44
+#define FLASH_CMD_PROGRAM_SEC_REG			0x42
+#define FLASH_CMD_READ_SEC_REG				0x48
+
+#define FLASH_CMD_GLOB_BLK_LOCK				0x7E
+#define FLASH_CMD_GLOB_BLK_UNLOCK			0x98
+#define FLASH_CMD_READ_BLK_LOCK				0x3D
+#define FLASH_CMD_INDIV_BLK_LOCK			0x36
+#define FLASH_CMD_INDIV_BLK_UNLOCK		0x39
+
+#define FLASH_CMD_ERASE_PROG_SUSPEND	0x75
+#define FLASH_CMD_ERASE_PROG_RESUME		0x7A
+#define FLASH_CMD_POWER_DOWN					0xB9
+
+#define FLASH_CMD_ENABLE_RESET				0x66
+#define FLASH_CMD_RESET_DEVICE				0x99
+
+#endif
---
+++ FreeRTOS_PoC_F4/Core/Src/flash.c
@@ -0,0 +1,62 @@
+/*
+ * flash.c
+ *
+ * Flash driver for W25Q16 serial Flash
+ *
+ */
+
+#include <stdint.h>
+#include "cmsis_os.h"
+#include "flash.h"
+#include "main.h"
+
+extern SPI_HandleTypeDef hspi1;
+
+uint8_t spiIoBuf[10];
+
+
+// internal functions
+// ==================
+
+void FlashCsSelect(void)
+{
+	HAL_GPIO_WritePin (GPIOB, GPIO_PIN_14, GPIO_PIN_RESET);
+}
+
+void FlashCsDeselect(void)
+{
+	HAL_GPIO_WritePin (GPIOB, GPIO_PIN_14, GPIO_PIN_SET);
+}
+
+void SPI_Write(uint8_t *data, uint8_t len)
+{
+	HAL_SPI_Transmit(&hspi1, data, len, 2000);
+}
+
+void SPI_Read(uint8_t *data, uint8_t len)
+{
+	HAL_SPI_Receive(&hspi1, data, len, 5000);
+}
+
+// exposed functions
+// =================
+
+void FlashReset(void)
+{
+	spiIoBuf[0] = FLASH_CMD_ENABLE_RESET;
+	spiIoBuf[1] = FLASH_CMD_RESET_DEVICE;
+	FlashCsSelect();
+	SPI_Write(spiIoBuf, 2);
+	FlashCsDeselect();
+	osDelay(100);
+}
+
+uint32_t W25Q_ReadID(void)
+{
+	spiIoBuf[0] = FLASH_CMD_READ_JEDEC_ID;
+	FlashCsSelect();
+	SPI_Write(spiIoBuf, 1);
+	SPI_Read(spiIoBuf, 3);
+	FlashCsDeselect();
+	return ((spiIoBuf[0]<<16)|(spiIoBuf[1]<<8)|spiIoBuf[2]);
+}
