5b30ba33a141a359cfb4b16b1fdc36a7756c12e1
Alexander <aperkals@gmail.com>
2026-07-19 12:55:31 +0300

    Moved common TLV definitions to the shared folder

--- FreeRTOS_PoC_F4/App/Inc/protocol.h
+++ FreeRTOS_PoC_F4/App/Inc/protocol.h
@@ -3,55 +3,10 @@
 
 #include <stdint.h>
 #include "flash.h"
-
-/*
-						TLV Protocol Definitions
-						========================
-
-
-           Structure of Rx packets
-           -----------------------
-
-			+------+--------+-----+----------+--------+
-			| Type | Length | Seq | Payload  | CRC32  |
-			+------+--------+-----+----------+--------+
-			 1 B      2 B     2 B    N B       4 B
-
-	 Type: command type (0 - 0xFF)
-	 Length: payload length. Up to RAW_PACKET_BUF_SIZE - 9
-	 Seq: sequence number (0 - 0xFFFF)
-	 Payload: data sent (*see Length)
-	 CRC32: checksum, according to STM32 hardware implementation
-
-           Structure of Tx packets
-           -----------------------
-
-			+------+--------+-----+--------+----------+--------+
-			| Type | Length | Seq | Status | Payload  | CRC32  |
-			+------+--------+-----+--------+----------+--------+
-			 1 B      2 B     2 B    2 B       N B       4 B
-
-	 Type: command type (0 - 0xFF)
-	 Length: payload length. Up to RAW_PACKET_BUF_SIZE - 11
-	 Seq: sequence number (0 - 0xFFFF)
-	 Status: status of command execution
-	 Payload: data sent (*see Length)
-	 CRC32: checksum, according to STM32 hardware implementation
- *
- */
+#include "tlv_common.h"
 
 #define RX_RING_SIZE           1024 // ring buffer for collecting Rx packets
 
-#define TLV_TYPE_SIZE    1
-#define TLV_LENGTH_SIZE  2
-#define TLV_SEQ_SIZE     2
-#define TLV_STATUS_SIZE  2
-#define TLV_CRC_SIZE     4
-
-#define TLV_RX_HEADER_SIZE (TLV_TYPE_SIZE + TLV_LENGTH_SIZE + TLV_SEQ_SIZE)
-#define TLV_RX_FOOTER_SIZE TLV_CRC_SIZE
-#define TLV_TX_HEADER_SIZE (TLV_TYPE_SIZE + TLV_LENGTH_SIZE + TLV_SEQ_SIZE + TLV_STATUS_SIZE)
-#define TLV_TX_FOOTER_SIZE TLV_CRC_SIZE
 
 // buffers to send/receive data from/to USB stack
 // ----------------------------------------------
@@ -67,26 +22,6 @@
 // division remainder
 #define CRC_CALC_BUF_SIZE (((TLV_MAX_TX_PAYLOAD_SIZE + TLV_TX_HEADER_SIZE) / 4) + 1)
 #define USB_TX_BUSY_WAIT_TIMEOUT_MS  100
-
-// TLV commands
-// ============
-#define CMD_GET_FW_VERSION     0x01
-#define CMD_GET_FLASH_ID       0x02
-#define CMD_READ_FLASH         0x03
-#define CMD_WRITE_FLASH        0x04
-#define CMD_TEST_1             0x05
-#define CMD_TEST_2             0x06
-#define CMD_TEST_3             0x07
-#define CMD_TEST_4             0x08
-#define CMD_TEST_5             0x09
-
-
-// TLV response statuses
-#define TLV_STAT_OK         0x0000
-#define TLV_STAT_TIMEOUT    0x0001
-#define TLV_STAT_NOT_IMPLEMENTED	0x0002
-#define TLV_STAT_INVALID_ARGUMENT 0X0003
-#define TLV_STAT_FAILURE		0x0004
 
 // Data structures
 
